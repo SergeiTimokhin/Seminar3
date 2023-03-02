ea1d55abf5460f79459931c9d47af389e4aa9b74
FirLanPro <FirLanPro@yandex.ru>
2023-03-02 10:49:09 +0300

    код

--- Ex025/Program.cs
+++ Ex025/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Numbers (int N)
+{
+   string strok =String.Empty;
+ int a = 1;
+ while (a <= N)
+    {
+        int result=a*a;
+        strok=strok+result + " ";
+        a = a + 1;
+    }
+    return strok;
+}
+    Console.Write("Введите число: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    string vivod = Numbers (N);
+    System.Console.WriteLine(vivod);
